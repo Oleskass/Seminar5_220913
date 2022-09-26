88d45926e1b73bdc98cd5393e1f942486bf3aa1b
Olesya Solovyova <soe611@yandex.ru>
2022-09-26 19:33:48 +0300

    modernize with doubles  Task 38 HW

--- Sem5Task38 HW/Program.cs
+++ Sem5Task38 HW/Program.cs
@@ -7,6 +7,7 @@
 // использовать заполнение массива целыми числами.
 // ========================================================================
 
+// с вещественными числами
 //чтение данных пользователя
 int ReadData(string line)
 {
@@ -25,23 +26,23 @@ void PrintResult(string line)
 }
 
 //генерация массива
-int[] RandomArr(int length)
+double[] RandomArr(int length)
 {
-    int[] array = new int[length];
+    double[] array = new double[length];
     Random rnd = new Random();
     for (int i = 0; i < length; i++)
     {
-        array[i] = rnd.Next(0, 100);
+        array[i] = Math.Round(rnd.NextDouble() * 100, 3); //умножила на 100, чтобы числа были от 0 до 100; округлила число до тысячных)
     }
     return array;
 }
 
 //генерация массива, поиск максимального и минимального значений и их разница
-string DiffMaxMin(int[] array)
+string DiffMaxMin(double[] array)
 {
-    int maxNum = array[0];
-    int minNum = array[0];
-    int diffNum = 0;
+    double maxNum = array[0];
+    double minNum = array[0];
+    double diffNum = 0;
 
     for (int i = 1; i < array.Length; i++) //цикл поиска минимального и максимального значений
     {
@@ -55,3 +56,54 @@ string DiffMaxMin(int[] array)
 
 //сама программа
 PrintResult(DiffMaxMin(RandomArr(ReadData("Введите длину массива: "))));
+
+
+// с целыми числами
+// //чтение данных пользователя
+// int ReadData(string line)
+// {
+//     //выводим сообщение
+//     Console.WriteLine(line);
+//     //считываем число
+//     int number = int.Parse(Console.ReadLine() ?? "0");
+//     //возвращаем значение
+//     return number;
+// }
+
+// //вывод результата
+// void PrintResult(string line)
+// {
+//     Console.WriteLine(line);
+// }
+
+// //генерация массива
+// int[] RandomArr(int length)
+// {
+//     int[] array = new int[length];
+//     Random rnd = new Random();
+//     for (int i = 0; i < length; i++)
+//     {
+//         array[i] = rnd.Next(0, 100);
+//     }
+//     return array;
+// }
+
+// //генерация массива, поиск максимального и минимального значений и их разница
+// string DiffMaxMin(int[] array)
+// {
+//     int maxNum = array[0];
+//     int minNum = array[0];
+//     int diffNum = 0;
+
+//     for (int i = 1; i < array.Length; i++) //цикл поиска минимального и максимального значений
+//     {
+//         if (array[i] > maxNum) maxNum = array[i];
+//         if (array[i] < minNum) minNum = array[i];
+//     }
+//     diffNum = maxNum - minNum;
+
+//     return (string.Join(", ", array) + "\nМаксимальное число: " + maxNum + "\nМинимальное число: " + minNum + "\nРазница между макс. и мин. : " + diffNum);
+// }
+
+// //сама программа
+// PrintResult(DiffMaxMin(RandomArr(ReadData("Введите длину массива: "))));
